62dd9bb39eab475a2407ebc8e435764b0db48955
Juan Felipe López <juanfelipelopez55@gmail.com>
2024-04-08 00:38:14 -0500

    feat: :sparkles: project finished, def theme implemented and functionalities

--- lib/presentation/screens/counters/counter_functions_screen.dart
+++ lib/presentation/screens/counters/counter_functions_screen.dart
@@ -13,50 +13,95 @@ class _CounterFunctionsScreenState extends State<CounterFunctionsScreen> {
   Widget build(BuildContext context) {
     //return const Placeholder(); //widget con x que muestra dónde se va a trabajar
     return Scaffold(
-      appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 36, 125, 120),
-          title: const Text(
-            "Counter Functions",
-            style: TextStyle(color: Color.fromARGB(255, 42, 7, 53)),
+        appBar: AppBar(
+            backgroundColor: const Color.fromARGB(255, 36, 125, 120),
+            title: const Text(
+              "Counter Functions",
+              style: TextStyle(color: Colors.white),
+            ),
+            actions: [
+              IconButton(
+                color: Colors.white,
+                onPressed: () {
+                  setState(() {
+                    clickCounter = 0;
+                  });
+                },
+                icon: const Icon(Icons.refresh_outlined),
+              ),
+            ]),
+        backgroundColor: const Color.fromARGB(255, 59, 146, 142),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("$clickCounter",
+                  style: const TextStyle(
+                      fontSize: 160,
+                      fontWeight: FontWeight.w200,
+                      color: Colors.white)),
+              Text("Click${clickCounter == 1 ? "" : "s"}",
+                  style: const TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w200,
+                      color: Colors.white))
+            ],
           ),
-          actions: [
-            IconButton(
+        ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            CustomButton(
+              icon: Icons.refresh_outlined,
               onPressed: () {
                 setState(() {
                   clickCounter = 0;
                 });
               },
-              icon: const Icon(Icons.refresh_outlined),
             ),
-          ]),
-      backgroundColor: const Color.fromARGB(255, 36, 125, 120),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("$clickCounter",
-                style: const TextStyle(
-                    fontSize: 160,
-                    fontWeight: FontWeight.w200,
-                    color: Colors.white)),
-            Text("Click${clickCounter == 1 ? "" : "s"}",
-                style: const TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.w200,
-                    color: Colors.white))
+            const SizedBox(height: 10),
+            CustomButton(
+              icon: Icons.exposure_minus_1_outlined,
+              onPressed: () {
+                setState(() {
+                  if (clickCounter == 0) return;
+                  clickCounter--;
+                });
+              },
+            ),
+            const SizedBox(height: 10), //para hacer un espacio entre ambos
+            CustomButton(
+              icon: Icons.plus_one_outlined,
+              onPressed: () {
+                setState(() {
+                  clickCounter++;
+                });
+              },
+            ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.black,
-        onPressed: () {
-          setState(() {
-            clickCounter++;
-          });
-        },
-        child:
-            const Icon(Icons.plus_one_rounded, color: Colors.white, size: 30),
-      ),
+        ));
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final VoidCallback? onPressed;
+
+  const CustomButton({
+    super.key,
+    required this.icon,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      enableFeedback: true, //sonido o vibración jaja
+      elevation: 105,
+      shape: const StadiumBorder(),
+      backgroundColor: Colors.white,
+      onPressed: onPressed,
+      child: Icon(icon, color: Color.fromARGB(255, 14, 171, 174), size: 30),
     );
   }
 }
